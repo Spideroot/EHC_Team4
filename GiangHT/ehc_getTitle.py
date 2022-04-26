36700d6e1987efebabc6e765073da7b8062527ee
GiangHT <95273832+perplexedG3@users.noreply.github.com>
2022-04-27 01:48:32 +0700

    Update ehc_getTitle.py

--- GiangHT/ehc_getTitle.py
+++ GiangHT/ehc_getTitle.py
@@ -3,25 +3,24 @@ from bs4 import BeautifulSoup
  
 url = "https://dantri.com.vn/the-thao.htm"
  
-
 r = requests.get(url)
  
 soup = BeautifulSoup(r.content, 'html.parser')
  
+temp1 = soup.select_one('div.row')
+
+temp2 = soup.select_one('div.row.line.mt-30')
+temp3 = temp2.select_one('div.col.col-840')
+
+result = temp1.select('h3.article-title') + temp3.select('h3.article-title')
+
 print("============TITLE=============")
-
 print(soup.title.text)
-
-temp = soup.select_one('div.row')
-
-temp2 = temp.select('h3.article-title')
-
-for i in temp2:
+count = 0
+for i in result:
     print(i.text)
+    count+=1
 
-t = soup.select_one('div.row.line.mt-30')
-t2 = t.select_one('div.col.col-840')
-t3 = t2.select('h3.article-title')
+print('\nTotal-title: ',count + 1)
+   
 
-for z in t3:
-    print(z.text)
